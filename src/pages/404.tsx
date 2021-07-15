2b08ada173622b02be9b627c7762da779caf6dea
raydim <snagin2004@gmail.com>
2021-07-15 23:23:46 +0300

    fix: fix 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -19,8 +19,11 @@ const Body = styled.div`
 `;
 
 const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
   background: #fff;
 `;
 
