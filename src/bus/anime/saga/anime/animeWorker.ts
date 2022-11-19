d08c45970f1340be272d6151eb8357dce788e701
sieugene <sieugene@mail.ru>
2022-11-20 00:10:07 +0900

    fix : checkout data

--- src/bus/anime/saga/anime/animeWorker.ts
+++ src/bus/anime/saga/anime/animeWorker.ts
@@ -1,23 +1,25 @@
+import { AnimeItemExtended } from 'bus/schedule/types';
+import { sheduleListStorage } from "bus/UserAnimeList/storage/sheduleList.storage";
 import { DetailsAnimeList, RawAnimeListType } from "bus/UserAnimeList/types";
 import { SagaIterator } from "redux-saga";
 import { call, put, select } from "redux-saga/effects";
 import service from "Services";
-import { sheduleListStorage } from "bus/UserAnimeList/storage/sheduleList.storage";
+import { formatAnimeDetail } from "../../../../utils/anime/formatAnimeDetail";
 import { setAnime, setErrorAnime, startAnime, stopAnime } from "../../actions";
 import { AnimeResponse, getAnimeType } from "../../types";
-import { formatAnimeDetail } from "../../../../utils/anime/formatAnimeDetail";
 
 export function* animeWorker(action: getAnimeType): SagaIterator {
   yield put(startAnime());
   const storage = sheduleListStorage();
   const { sheduleList } = storage.get();
   const { selectedDay } = yield select((state) => state.anime);
-  let anime = null;
-  anime = Object.values(sheduleList)?.reduce((acc, el) => {
+
+  const anime = sheduleList && Object.values(sheduleList)?.reduce((acc, el) => {
     const findAnime = el?.find((el) => el._id === action?.payload);
     if (findAnime && Object.keys(findAnime).length !== 0) acc = findAnime;
     return acc;
-  }, {});
+  }, {} as AnimeItemExtended);
+
   if (!!anime && Object.keys(anime).length !== 0) {
     try {
       yield put(setAnime({ anime, detail: [] }));
